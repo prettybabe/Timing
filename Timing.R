b66acb76f3fb1a3aaee908f043c7bf33d45d5ef7
prettybabe <prettybabe@126.com>
2015-05-13 18:34:29 +0800

    到计算Score部分

--- Timing.R
+++ Timing.R
@@ -10,6 +10,12 @@ library(rCharts)
 library(xlsx)
 
 source('D:/working/Timing/MyFunction.R')
+channel <- src_sqlserver(server="SQL",database="XY",user="libo.jin",password="123456")
+tradingDay <- tbl(channel, "QT_TradingDayNew") %>%
+  filter(SecuMarket == 83L, IfTradingDay==1L) %>%
+  select(TradingDate, IfWeekEnd, IfMonthEnd, IfQuarterEnd, IfYearEnd) %>%
+  collect %>%
+  mutate(TradingDate = as.Date(TradingDate))
 #############################################################################################################
 #  原始数据 
 data <- list()
@@ -24,11 +30,11 @@ data$IF00 <- read.xlsx("D:\\working\\Timing\\数据.xlsx", sheetIndex = 2, start
 
 data$Nasdaq <- read.xlsx("D:\\working\\Timing\\数据.xlsx", sheetIndex = 3, startRow = 3,
                        header = TRUE, encoding = "UTF-8") %>%
-  rename(Date = 日期, PreClosePrice =  前收盘价, ClosePrice = 收盘价)
+  rename(Date = 日期, ClosePrice = 收盘价)
 
 data$SP <- read.xlsx("D:\\working\\Timing\\数据.xlsx", sheetIndex = 4, startRow = 3,
                        header = TRUE, encoding = "UTF-8") %>%
-  rename(Date = 日期, PreClosePrice =  前收盘价, ClosePrice = 收盘价)
+  rename(Date = 日期, ClosePrice = 收盘价)
 
 data$Yield <- read.xlsx("D:\\working\\Timing\\数据.xlsx", sheetIndex = 5, startRow = 2,
                        header = TRUE, encoding = "UTF-8") %>%
@@ -56,6 +62,12 @@ data$IP <- read.xlsx("D:\\working\\Timing\\数据.xlsx", sheetIndex = 10, startR
                      header = TRUE, encoding = "UTF-8") %>%
   rename(Date = 指标名称, IP =  工业增加值.当月同比, InfoPublicDate = 发布时间)
 
+data$Value <- read.xlsx("D:\\working\\Timing\\数据.xlsx", sheetIndex = 11,
+                     header = TRUE, encoding = "UTF-8") 
+
+data$Rsquare <- read.xlsx("D:\\working\\Timing\\数据.xlsx", sheetIndex = 12,
+                     header = TRUE, encoding = "UTF-8")
+
 #############################################################################################################
 # 处理数据
 # 日度数据
@@ -64,11 +76,23 @@ data_day$Momentum <- data$Index %>%
   mutate(Return =  ClosePrice/PreClosePrice - 1) %>%
   mutate(Momentum15D = Momentum(Return, 15)) %>%
   select(Date, Momentum15D)
+
+data_day$SP <- data$SP %>%
+  mutate(Return = ClosePrice/lag(ClosePrice, 5) - 1) %>%
+  select(Date, Return)
+data_day$SP <- data.frame(Date = data$Index$Date, SP = AdjustUSA(data$Index$Date, data_day$SP))
+ 
+
+data_day$Nasdaq <- data$Nasdaq %>%
+  mutate(Return = ClosePrice/lag(ClosePrice, 5) - 1) %>%
+  select(Date, Return)
+data_day$Nasdaq <- data.frame(Date = data$Index$Date, Nasdaq = AdjustUSA(data$Index$Date, data_day$Nasdaq))
+
   
 data_day$Volatility <- data$Index %>%
   mutate(Return =  ClosePrice/PreClosePrice - 1) %>%
-  mutate(Momentum10D = Volatility(Return, 10), Momentum5D = Volatility(Return, 5)) %>%
-  select(Date, Momentum5D, Momentum10D)
+  mutate(Volatility10D = Volatility(Return, 10), Volatility5D = Volatility(Return, 5)) %>%
+  select(Date, Volatility5D, Volatility10D)
 
 data_day$Volume <- data$Index %>%
   mutate(Volume3d = Average(TurnoverVolume, 3), Volume5d = Average(TurnoverVolume, 5),
@@ -80,14 +104,29 @@ data_day$Yield <- data$Yield %>%
          TwoYearChange = TwoYear - lag(TwoYear, 5), FiveYearChange = FiveYear - lag(FiveYear, 5),
          TenYearChange = TenYear - lag(TenYear, 5), TenYearMinusTwoYearChange = TenYearMinusTwoYear - lag(TenYearMinusTwoYear, 5),
          FiveYearMinusTwoYearChange = FiveYearMinusTwoYear - lag(FiveYearMinusTwoYear, 5)) 
+data_day$Yield <- data.frame(Date = data$Index$Date, 
+                             TwoYear = AdjustUSA(data$Index$Date, select(data_day$Yield, Date, TwoYear)),
+                             TwoYearChange = AdjustUSA(data$Index$Date, select(data_day$Yield, Date, TwoYearChange)),
+                             FiveYear = AdjustUSA(data$Index$Date, select(data_day$Yield, Date, FiveYear)),
+                             FiveYearChange = AdjustUSA(data$Index$Date, select(data_day$Yield, Date, FiveYearChange)),
+                             TenYear = AdjustUSA(data$Index$Date, select(data_day$Yield, Date, TenYear)),
+                             TenYearChange = AdjustUSA(data$Index$Date, select(data_day$Yield, Date, TenYearChange)),
+                             FiveYearMinusTwoYear = AdjustUSA(data$Index$Date, select(data_day$Yield, Date, FiveYearMinusTwoYear)),
+                             FiveYearMinusTwoYearChange = AdjustUSA(data$Index$Date, select(data_day$Yield, Date, FiveYearMinusTwoYearChange)),
+                             TenYearMinusTwoYear = AdjustUSA(data$Index$Date, select(data_day$Yield, Date, TenYearMinusTwoYear)),
+                             TenYearMinusTwoYearChange = AdjustUSA(data$Index$Date, select(data_day$Yield, Date, TenYearMinusTwoYearChange)))
+                             
+
+
+
 
 data_day$CloseMinusSettle <- data$IF00 %>%
   mutate(CloseMinusSettle = ClosePrice - SettlementPrice) %>%
   select(Date, CloseMinusSettle)
 
-data_day$Value <- 
+data_day$Value <- data$Value
 
-data_day$Sentiments <- 
+data_day$Rsquare <- data$Rsquare
 
 #################################################################################################################
 # 月度数据 
@@ -118,5 +157,31 @@ data_month$IP <- data$IP %>%
 data_month$IP$AdjustInfoPublicDate <- as.Date(AdjustDate(data$Index$Date, data_month$IP$InfoPublicDate))
 
 ###########################################################################################################
-# 周度数据
+# 日期序列
+date_weekly <- tradingDay %>% 
+  filter(IfWeekEnd == 1) %>%
+  arrange(TradingDate) %>%
+  mutate(Lag = lag(TradingDate, 1)) %>%
+  select(TradingDay = Lag, ForecastDay = TradingDate) %>% 
+  filter(!is.na(TradingDay))
 
+##########################################################################################################
+# 周度数据
+data_week <- list()
+
+data_week$UnMinusMean <- data_day$Momentum %>%
+  semi_join(date_weekly, by = c("Date" = "TradingDay")) %>%
+  left_join(data_day$SP, by = "Date") %>%
+  left_join(data_day$Nasdaq, by = "Date")
+
+data_week$MinusMean <- data_day$Volatility %>%
+  semi_join(date_weekly, by = c("Date" = "TradingDay")) %>%
+  left_join(data_day$Value, by = "Date") %>%
+  left_join(data_day$Rsquare, by = "Date") %>%
+  left_join(data_day$CloseMinusSettle,  by = "Date") %>%
+  left_join(data_day$Yield, by = "Date")
+
+#########################################################################################################
+# 
+
+score <-
